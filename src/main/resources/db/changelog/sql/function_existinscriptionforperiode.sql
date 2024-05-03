931aa7c7668274d313bc24b5d6d6e290a9675436
Aymen <aymen330d@hotmail.fr>
2024-05-03 21:53:59 +0200

    fix changelog

---
+++ src/main/resources/db/changelog/sql/function_existinscriptionforperiode.sql
@@ -0,0 +1,22 @@
+create or replace function moth.existInscriptionForPeriode(idPeriode int8)
+returns boolean
+language plpgsql
+as
+$$
+declare
+   idElev int8;
+begin
+   select elev.idElev
+   into idElev
+   from moth.eleve elev
+   inner join moth.tarif tari on tari.idtari = elev.idtari
+   inner join moth.periode peri on peri.idperi = tari.idperi
+   where
+   peri.idPeri = idPeriode
+   limit 1;
+
+  IF idElev > 0 THEN
+   return true;
+  else return false;
+ end if;
+end;
