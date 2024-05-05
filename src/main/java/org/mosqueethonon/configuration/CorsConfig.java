b66c18343b4a8a6aad1b2a4a845e5bfbea354525
Aymen <aymen330d@hotmail.fr>
2024-05-05 22:40:28 +0200

    security CORS

--- src/main/java/org/mosqueethonon/configuration/CorsConfig.java
+++ src/main/java/org/mosqueethonon/configuration/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://inscription-amc.fr", "https://inscription-mosquee-thonon.org")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
