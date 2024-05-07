9ef3970c58fc5d85afc5e885f9fef63e6c3c4e5f
Aymen <aymen330d@hotmail.fr>
2024-05-07 19:24:55 +0200

    CORS prod domain

--- src/main/java/org/mosqueethonon/configuration/CorsConfigProd.java
+++ src/main/java/org/mosqueethonon/configuration/CorsConfigProd.java
@@ -12,7 +12,7 @@ public class CorsConfigProd implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://www.inscription-amc.fr", "https://www.inscription-mosquee-thonon.org")
+                .allowedOrigins("https://inscription-amc.fr", "https://inscription-mosquee-thonon.org")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
