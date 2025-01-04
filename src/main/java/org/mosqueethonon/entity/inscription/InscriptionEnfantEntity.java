f075d89189397a7d4851b9201b372c352faef355
Aymen <aymen330d@hotmail.fr>
2025-01-04 19:17:12 +0100

    removing deprecated APIs

--- src/main/java/org/mosqueethonon/entity/inscription/InscriptionEnfantEntity.java
+++ src/main/java/org/mosqueethonon/entity/inscription/InscriptionEnfantEntity.java
@@ -3,10 +3,12 @@ package org.mosqueethonon.entity.inscription;
 import lombok.Data;
 
 import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
 
 @Entity
 @DiscriminatorValue("ENFANT")
 @Data
+@EqualsAndHashCode(callSuper = true)
 public class InscriptionEnfantEntity extends InscriptionEntity {
 
 }
